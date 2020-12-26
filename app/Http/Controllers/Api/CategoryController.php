74acd3aeca7794bde6bd3bef6ec6d082acc49fca
aminur <aminurrashid126@gmail.com>
2020-12-26 22:42:09 +0600

    category api

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -2,10 +2,109 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Category;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CategoryRequest;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use DB;
 
 class CategoryController extends Controller
 {
-    //
+    public function index()
+    {
+        //all category come in my table
+        $category = Category::latest()->get();
+
+        return response()->json([
+            'categories' => $category,
+            'status_code' => 200
+        ], Response::HTTP_OK);
+    }
+
+    public function store(CategoryRequest $request)
+    {
+        if ($request->isMethod('post'))
+        {
+            DB::beginTransaction();
+
+            try{
+                // Step 1 : Create Category
+
+                $category = new Category();
+                $category->cat_name = $request->cat_name;
+                $category->cat_slug = strtolower($request->cat_name);
+
+                $category->save();
+
+                DB::commit();
+
+                return response()->json([
+                    'message' => 'Category Added Successfully'
+                ],200);
+
+            }catch(\Illuminate\Database\QueryException $e){
+                DB::rollback();
+                $error = $e->getMessage();
+
+                return response()->json([
+                    'error' => $error
+                ],500);
+            }
+        }
+    }
+
+    public function edit($id)
+    {
+        $category = Category::findOrFail($id);
+
+        return \response()->json([
+            'edit_category' => $category,
+            'status_code' => 200
+        ],Response::HTTP_OK);
+    }
+
+    public function update(CategoryRequest $request,$id)
+    {
+        if ($request->isMethod('post'))
+        {
+            DB::beginTransaction();
+
+            try{
+                // Step 1 : Create Category
+
+                $category = Category::findOrFail($id);
+
+                $category->cat_name = $request->cat_name;
+                $category->cat_slug = strtolower($request->cat_name);
+
+                $category->save();
+
+                DB::commit();
+
+                return response()->json([
+                    'message' => 'Category Updated Successfully'
+                ],200);
+
+            }catch(\Illuminate\Database\QueryException $e){
+                DB::rollback();
+                $error = $e->getMessage();
+
+                return response()->json([
+                    'error' => $error
+                ],500);
+            }
+        }
+    }
+
+    public function destroy($id)
+    {
+        $category = Category::findOrfail($id);
+        $category->delete();
+
+        return \response()->json([
+            'message' => 'category Deleted Successfully',
+            'status_code' => 200
+        ], Response::HTTP_OK);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -12,5 +12,19 @@ Route::group(['prefix' => 'auth', 'namespace' => 'Auth'], function (){
         Route::get('me', 'MeController');
     });
 
+});
 
+// Ecommerce Main Layouts Routes For Customers
+
+// Customers Auth Routes
+
+// DashBoard Routes
+Route::group(['middleware' => 'auth:api', 'namespace' => 'Api'], function (){
+
+    //Category Routes
+    Route::get('category', 'CategoryController@index');
+    Route::post('category/store', 'CategoryController@store');
+    Route::get('category/edit/{id}', 'CategoryController@edit');
+    Route::post('category/update/{id}', 'CategoryController@update');
+    Route::delete('category/destroy/{id}', 'CategoryController@destroy');
 });
